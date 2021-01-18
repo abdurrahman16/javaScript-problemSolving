771a1bb148aca01c7e14de6b439486d263610db0
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-18 23:08:11 +0600

    es6

--- problemsolving.js
+++ problemsolving.js
@@ -1,9 +1,22 @@
-function inchToFeet (inch){
-    var feet= inch/12;
-    return feet;
+// icnh to feet
+
+// function inchToFeet (inch){
+//     var feet= inch/12;
+//     return feet;
+// }
+// var nafi= [120,144,244];
+// var result= inchToFeet(nafi[1]);
+// console.log(result);
+
+var nam ='ma';
+
+if(nam.length<3)
+{
+    nam='Babu';
+    console.log(nam);
+}
+else{
+    
+    console.log(nam);
 }
 
-var nafi= [120,144,244];
-
-var result= inchToFeet(nafi[1]);
-console.log(result);
