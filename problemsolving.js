e1a74b748deb244c3f86a60b4fdfac7a3d5e4a9f
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-18 23:41:26 +0600

    es6

--- problemsolving.js
+++ problemsolving.js
@@ -8,15 +8,46 @@
 // var result= inchToFeet(nafi[1]);
 // console.log(result);
 
-var nam ='ma';
+//es6
+// let nam ='ma';
 
-if(nam.length<3)
-{
-    nam='Babu';
-    console.log(nam);
-}
-else{
+// if(nam.length<3)
+// {
+//     nam='Babu';
+//     console.log(nam);
+// }
+// else{
     
-    console.log(nam);
+//     console.log(nam);
+
+//leap year
+
+// console.log('Leap Year Program');
+// let year= prompt('enter the year');
+// console.log(year);
+
+// if ( year%4==0 && (year/400==0))
+// {
+//   console.log('leap year');
+// }
+
+// else {
+//     console.log('NL');
+// }
+
+
+function leapYear (year) 
+{
+    if((year%4==0) &&(year%400==0))
+     {
+        let result= 'leap year';
+    }
+    else{
+        result = 'NL';
+    }
+
+    return result;
 }
 
+let r= leapYear(4);
+console.log(r);
